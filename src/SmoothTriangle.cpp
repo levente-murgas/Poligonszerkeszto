1fc6af5292d9bf68aa40ffad0a0f470910916f5a
levente-murgas <murgaslevi@gmail.com>
2022-11-05 21:26:11 +0100

    masodik hazi in progress

--- src/SmoothTriangle.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by murga on 10/8/2022.
-//
--- src/guestSkeleton.cpp
+++ src/guestSkeleton.cpp
@@ -1,8 +1,8 @@
 //=============================================================================================
 // NYILATKOZAT
 // ---------------------------------------------------------------------------------------------
-// Nev    : Murgás Levente
-// Neptun : M5E2T9
+// Nev    : Demeter Zalán
+// Neptun : VERF1U
 // ---------------------------------------------------------------------------------------------
 // ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
 // mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
@@ -16,15 +16,12 @@
 //=============================================================================================
 #include "framework.h"
 
-bool isInside(vec3 normal, vec3 p, vec3 r1, vec3 r2){
-    return dot(cross(r2-r1,p-r1),normal) > 0;
-}
-
 struct Material {
-    vec3 ka, kd, ks, n, kappa;
+    vec3 ka, kd, ks;
     float  shininess;
     vec3 F0;
-    int rough, reflective, refractive;
+    int rough, reflective;
+    int portal = false;
 };
 
 struct RoughMaterial : Material {
@@ -35,25 +32,17 @@ struct RoughMaterial : Material {
         shininess = _shininess;
         rough = true;
         reflective = false;
-        refractive = false;
     }
 };
 
 inline vec3 operator/(const vec3& v1, const vec3& v2) { return vec3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z); }
 
-struct RefractiveAndRoughMaterial : Material {
-    RefractiveAndRoughMaterial(vec3 _n, vec3 _kappa,vec3 _kd, vec3 _ks, float _shininess) {
-        n = _n;
-        kappa = _kappa;
-        ka = _kd * M_PI;
-        kd = _kd;
-        ks = _ks;
-        shininess = _shininess;
+struct SmoothMaterial : Material {
+    SmoothMaterial(vec3 n, vec3 kappa) {
         vec3 one(1, 1, 1);
         F0 = ((n - one) * (n - one) + kappa * kappa) / ((n + one) * (n + one) + kappa * kappa);
-        rough = true;
-        reflective = false;
-        refractive = true;
+        rough = false;
+        reflective = true;
     }
 };
 
@@ -66,12 +55,7 @@ struct Hit {
 
 struct Ray {
     vec3 start, dir;
-    bool out = true;
-    Ray(vec3 _start = vec3(), vec3 _dir = vec3(), bool _out = true) {
-        start = _start;
-        dir = normalize(_dir);
-        out = _out;
-    }
+    Ray(vec3 _start = vec3(), vec3 _dir = vec3()) { start = _start; dir = normalize(_dir); }
 };
 
 class Intersectable {
@@ -81,52 +65,38 @@ public:
     virtual Hit intersect(const Ray& ray) = 0;
 };
 
-struct Cube : public Intersectable {
-    struct face4 {
-        unsigned int i, j, k, l;
-        face4(unsigned int _i = 0, unsigned int _j = 0, unsigned int _k = 0, unsigned int _l = 0)
-                : i(_i - 1), j(_j - 1), k(_k - 1), l(_l - 1){}
-    };
-    std::vector<vec3> vertices; //8 vertices
-    std::vector<face4> faces; //6 faces
+struct Paraboloid : public Intersectable {
+    float a, b, c;
 
-    Cube(const std::vector<vec3>& _vertices, const std::vector<face4>& _faces, Material* _material) {
-        vertices = _vertices;
-        faces = _faces;
+    Paraboloid(float _a, float _b, float _c, Material* _material) {
+        a = _a; b = _b; c = _c;
         material = _material;
     }
 
-    Hit intersect(const Ray& ray){
+    Hit intersect(const Ray& ray) {
         Hit hit;
-        for (const face4& face: faces) {
-            vec3 p1 = vertices[face.i];
-            vec3 p2 = vertices[face.j];
-            vec3 p3 = vertices[face.k];
-            vec3 u = p2 - p1;
-            vec3 v = p3 - p1;
-            vec3 normal = normalize(cross(u, v));
-            float t = dot(normal,(p1 - ray.start)) / dot(normal, ray.dir);
-
-            if ((t > 0 && hit.t == -1) || (t > 0 && t < hit.t)) { //find the plane that the ray intersects first
-                vec3 p = ray.start + ray.dir * t;
-                vec3 A = vertices[face.i];
-                vec3 B = vertices[face.j];
-                vec3 C = vertices[face.k];
-                vec3 D = vertices[face.l];
-
-                bool b1 = isInside(normal,p,A,B);
-                bool b2 = isInside(normal,p,B,C);
-                bool b3 = isInside(normal,p,C,D);
-                bool b4 = isInside(normal,p,D,A);
-
-                if (b1 && b2 && b3 && b4) {
-                    hit.t = t;
-                    hit.position = p;
-                    hit.normal = normal;
-                    hit.material = material;
-                }
-            }
+        float A = a * (ray.dir.x * ray.dir.x) + b * (ray.dir.y * ray.dir.y);
+        float B = 2.0f * a * ray.start.x * ray.dir.x + 2.0f * b * ray.start.y * ray.dir.y - c * ray.dir.z;
+        float C = a * (ray.start.x * ray.start.x) + b * (ray.start.y * ray.start.y) - c * ray.start.z;
+        float discr = B * B - 4.0f * A * C;
+        if (discr < 0) return hit;
+        float sqrt_discr = sqrtf(discr);
+        float t1 = (-B + sqrt_discr) / 2.0f / A;
+        float t2 = (-B - sqrt_discr) / 2.0f / A;
+        if (t1 <= 0) return hit;
+        vec3 intersect1 = ray.start + ray.dir * t1;
+        vec3 intersect2 = ray.start + ray.dir * t2;
+        if (powf((intersect2.x), 2) + powf((intersect2.y), 2) + powf((intersect2.z), 2) <= 0.3 * 0.3) {
+            hit.t = t2;
+            hit.position = intersect2;
         }
+        else if (powf((intersect1.x), 2) + powf((intersect1.y), 2) + powf((intersect1.z), 2) <= 0.3 * 0.3) {
+            hit.t = t1;
+            hit.position = intersect1;
+        }
+        else return hit;
+        hit.normal = normalize(cross(vec3(1, 0, (2 * a * hit.position.x) / c), vec3(0, 1, (2 * b * hit.position.y) / c)));
+        hit.material = material;
         return hit;
     }
 };
@@ -142,14 +112,20 @@ struct Dodecahedron : public Intersectable {
     std::vector<face5> faces;
     Material* materialRefl;
 
-    Dodecahedron(const std::vector<vec3>& _vertices, const std::vector<face5>& _faces, Material* _material) {
+    Dodecahedron(const std::vector<vec3>& _vertices, const std::vector<face5>& _faces, Material* _material, Material* _materialRefl) {
         vertices = _vertices;
         faces = _faces;
         material = _material;
+        materialRefl = _materialRefl;
+    }
+
+    float getDist(Hit hit, vec3 A, vec3 B) {
+        return length(cross((hit.position - A), (hit.position - B))) / length(B - A);
     }
 
     Hit intersect(const Ray& ray) {
         Hit hit;
+        face5 currentFace;
         for (const face5& face: faces) {
             vec3 p1 = vertices[face.i];
             vec3 p2 = vertices[face.j];
@@ -158,29 +134,31 @@ struct Dodecahedron : public Intersectable {
             vec3 v = p3 - p1;
             vec3 normal = normalize(cross(u, v));
             float t = dot(normal,(p1 - ray.start)) / dot(normal, ray.dir);
-
-            if ((t > 0 && hit.t == -1) || (t > 0 && t < hit.t)) { //find the plane that the ray intersects first
-                vec3 p = ray.start + ray.dir * t;
-                vec3 A = vertices[face.i];
-                vec3 B = vertices[face.j];
-                vec3 C = vertices[face.k];
-                vec3 D = vertices[face.l];
-                vec3 E = vertices[face.m];
-
-                bool b1 = isInside(normal,p,A,B);
-                bool b2 = isInside(normal,p,B,C);
-                bool b3 = isInside(normal,p,C,D);
-                bool b4 = isInside(normal,p,D,E);
-                bool b5 = isInside(normal,p,E,A);
-
-                if (b1 && b2 && b3 && b4 && b5) {
-                    hit.t = t;
-                    hit.position = p;
-                    hit.normal = normal;
-                    hit.material = material;
-                }
+            if ((t > 0 && hit.t == -1) || (t > 0 && t < hit.t)) {
+                hit.t = t;
+                hit.position = ray.start + ray.dir * hit.t;
+                hit.normal = normal;
+                hit.material = material;
+                currentFace = face;
             }
         }
+
+        vec3 A = vertices[currentFace.i];
+        vec3 B = vertices[currentFace.j];
+        vec3 C = vertices[currentFace.k];
+        vec3 D = vertices[currentFace.l];
+        vec3 E = vertices[currentFace.m];
+
+        float d1 = getDist(hit, A, B);
+        float d2 = getDist(hit, B, C);
+        float d3 = getDist(hit, C, D);
+        float d4 = getDist(hit, D, E);
+        float d5 = getDist(hit, E, A);
+
+        if (d1 < 0.1 || d2 < 0.1 || d3 < 0.1 || d4 < 0.1 || d5 < 0.1)
+            hit.material = material;
+        else
+            hit.material = materialRefl;
         return hit;
     }
 };
@@ -194,9 +172,9 @@ public:
         lookat = _lookat;
         fov = _fov;
         vec3 w = eye - lookat;
-        float focus = length(w);
-        right = normalize(cross(vup, w)) * focus * tanf(fov / 2);
-        up = normalize(cross(w, right)) * focus * tanf(fov / 2);
+        float f = length(w);
+        right = normalize(cross(vup, w)) * f * tanf(fov / 2);
+        up = normalize(cross(w, right)) * f * tanf(fov / 2);
     }
 
     Ray getRay(int X, int Y) {
@@ -210,22 +188,21 @@ public:
                    -(eye.x - lookat.x) * sinf(dt) + (eye.z - lookat.z) * cosf(dt) + lookat.z);
         set(eye, lookat, up, fov);
     }
-
 };
 
 struct Light {
     vec3 position;
-    vec3 Le;
-    Light(vec3 _position = vec3(), vec3 _Le = vec3(1, 1, 1)) {
-        position = normalize(_position);
-        Le = _Le;
+    vec3 color;
+    Light(vec3 _position = vec3(), vec3 _color = vec3(1, 1, 1)) {
+        position = _position;
+        color = _color;
     }
     vec3 radiance(float distance) {
-        return Le / (powf(distance, 2));
+        return color / (powf(distance, 2));
     }
 };
 
-const float epsilon = 0.0001f;
+const float epsilon = 0.0001;
 const int maxdepth = 5;
 
 class Scene {
@@ -234,34 +211,17 @@ class Scene {
     Camera camera;
     vec3 La;
 public:
-
     void build() {
-        vec3 eye = vec3(-0.75, -0.8, 0.5), vup = vec3(0, 0, 1), lookat = vec3(0.5, 0.5, 0.5);
+        vec3 eye = vec3(0, 0, 1.35), vup = vec3(0, 1, 0), lookat = vec3(0, 0, 0);
         float fov = 45 * M_PI / 180;
         camera.set(eye, lookat, vup, fov);
-        //camera.Animate(0.885398);
 
-        La = vec3(0.2f,0.2f,0.2f);
-        Light *redLight = new Light(vec3(0,0.5,0),vec3(1,0,0));
-        Light *greenLight = new Light(vec3(0.5,0.5,0),vec3(0,1,0));
-        Light *blueLight = new Light(vec3(-0.5,0.5,0),vec3(0,0,1));
-        //lights.push_back(redLight);
-        //lights.push_back(greenLight);
-        //lights.push_back(blueLight);
+        La = vec3(0.75, 0.75, 0.75);
+        lights.push_back(new Light(vec3(0, 0.5, 0), vec3(1, 1, 1)));
 
-        //init cube
-        vec3 cubeVertexArr[] = {vec3(0,0,0), vec3(0,0,1), vec3(0,1,0), vec3(0,1,1),
-                                vec3(1,0,0), vec3(1,0,1), vec3(1,1,0), vec3(1,1,1)};
-        std::vector<vec3> cubeVertices(cubeVertexArr,cubeVertexArr + 8);
+        Material* gold = new SmoothMaterial(vec3(0.17, 0.35, 1.5), vec3(3.1, 2.7, 1.9));
+        objects.push_back(new Paraboloid(5, 5, 1, gold));
 
-        Cube::face4 cubeFaceArr[] = {Cube::face4(1,2,6,5),Cube::face4(3,4,2,1),
-                                     Cube::face4(7,8,4,3), Cube::face4(5,6,8,7),
-                                     Cube::face4(2,4,8,6), Cube::face4(1,3,7,5)};
-        std::vector<Cube::face4> cubeFaces(cubeFaceArr, cubeFaceArr + 6);
-        Material *refractiveAndRoughMaterial = new RefractiveAndRoughMaterial(vec3(1,1,1),vec3(1,1,1),vec3(0.5, 0.5, 0.5), vec3(0.1, 0.1, 0.1), 100);
-        objects.push_back(new Cube(cubeVertices,cubeFaces,refractiveAndRoughMaterial));
-
-        //init dodecahedron
         vec3 vertexArr[] = {vec3(0,0.618,1.618), vec3(0,-0.618,1.618),	vec3(0,-0.618,-1.618),	vec3(0,0.618,-1.618),
                             vec3(1.618,0,0.618), vec3(-1.618,0,0.618),	vec3(-1.618,0,-0.618),	vec3(1.618,0,-0.618),
                             vec3(0.618,1.618,0), vec3(-0.618,1.618,0),	vec3(-0.618,-1.618,0),	vec3(0.618,-1.618,0),
@@ -278,14 +238,16 @@ public:
         std::vector<Dodecahedron::face5> faces(faceArr, faceArr + 12);
 
         Material* ceramicPink = new RoughMaterial(vec3(0.3, 0.25, 0.3), vec3(2, 2, 2), 1000);
-        //objects.push_back(new Dodecahedron(vertices, faces, ceramicPink));
+        Material* zinc = new SmoothMaterial(vec3(1.2338, 0.92943, 0.67767), vec3(5.8730, 4.9751, 4.0122));
+        zinc->portal = true;
+        objects.push_back(new Dodecahedron(vertices, faces, ceramicPink, zinc));
     }
 
     void render(std::vector<vec4>& image) {
         for (int Y = 0; Y < windowHeight; Y++) {
 #pragma omp parallel for
             for (int X = 0; X < windowWidth; X++) {
-                vec3 color = trace(camera.getRay(X, Y));
+                vec3 color = trace(camera.getRay(X, Y), 0);
                 image[Y * windowWidth + X] = vec4(color.x, color.y, color.z, 1);
             }
         }
@@ -293,8 +255,8 @@ public:
 
     Hit firstIntersect(Ray ray) {
         Hit bestHit;
-        for (Intersectable * object : objects) {
-            Hit hit = object->intersect(ray); //  hit.t < 0 if no intersection
+        for (Intersectable* object : objects) {
+            Hit hit = object->intersect(ray);
             if (hit.t > 0 && (bestHit.t < 0 || hit.t < bestHit.t))  bestHit = hit;
         }
         if (dot(ray.dir, bestHit.normal) > 0) bestHit.normal = bestHit.normal * (-1);
@@ -305,13 +267,6 @@ public:
         return V - N * dot(N, V) * 2;
     };
 
-    vec3 refract(vec3 V, vec3 N, float ns){
-        float cosa = -dot(V,N);
-        float disc = 1 - (1-cosa*cosa) / ns / ns; //n scalar
-        if (disc < 0) return vec3(0,0,0);
-        return V/ns + N * (cosa/ns - sqrt(disc));
-    }
-
     vec3 Fresnel(vec3 V, vec3 N, vec3 F0) {
         float cosa = -dot(V, N);
         return F0 + (vec3(1, 1, 1) - F0) * powf(1 - cosa, 5);
@@ -320,11 +275,11 @@ public:
     vec3 trace(Ray ray, int d = 0) {
         if (d > maxdepth) return La;
         Hit hit = firstIntersect(ray);
-        if (hit.t < 0) return La; //La
+        if (hit.t < 0) return La;
         vec3 outRadiance = vec3(0, 0, 0);
 
         if (hit.material->rough) {
-            outRadiance = La * (1 + fabs(dot(hit.normal, ray.dir))); //L = 0.2 * (1 + dot(N, V) | 0.2 <= L =< 0.4
+            outRadiance = hit.material->ka * La;
             for (Light* light : lights) {
                 vec3 direction = normalize(light->position - hit.position);
                 float distance = length(light->position - hit.position);
@@ -344,30 +299,24 @@ public:
         if (hit.material->reflective) {
             vec3 reflectedDir = ray.dir - hit.normal * dot(hit.normal, ray.dir) * 2.0f;
             vec3 reflectedPoint = hit.position + hit.normal * epsilon;
-            Ray reflectRay(reflectedPoint,reflectedDir);
             float cosa = -dot(ray.dir, hit.normal);
             vec3 one(1, 1, 1);
             vec3 F = hit.material->F0 + (one - hit.material->F0) * powf(1 - cosa, 5);
-            outRadiance = outRadiance + F * trace(reflectRay, d + 1);
-        }
-
-        if(hit.material->refractive) {
-            float ior = (ray.out) ? hit.material->n.x : (1/hit.material->n.x);
-            vec3 refractionDir = refract(ray.dir,hit.normal,ior);
-            if (length(refractionDir) > 0) {
-                vec3 refractedPoint = hit.position - hit.normal * epsilon;
-                Ray refractRay(refractedPoint,refractionDir,!ray.out);
-                float cosa = -dot(ray.dir, hit.normal);
-                vec3 one(1, 1, 1);
-                vec3 F = hit.material->F0 + (one - hit.material->F0) * powf(1 - cosa, 5);
-                outRadiance = outRadiance + (vec3(1,1,1) - F) * trace(refractRay, d + 1);
+            if (hit.material->portal) {
+                float theta = 72 * M_PI / 180;
+                vec3 rotDir = reflectedDir * cosf(theta) + cross(hit.normal, reflectedDir) * sinf(theta) + hit.normal * dot(hit.normal, reflectedDir) * (1 - cosf(theta));
+                vec3 rotPoint = reflectedPoint * cosf(theta) + cross(hit.normal, reflectedPoint) * sinf(theta) + hit.normal * dot(hit.normal, reflectedPoint) * (1 - cosf(theta));
+                outRadiance = outRadiance + F * trace(Ray(rotPoint, rotDir), d + 1);
             }
-
+            else {
+                outRadiance = outRadiance + F * trace(Ray(reflectedPoint, reflectedDir), d + 1);
+            }
         }
 
         return outRadiance;
     }
 
+    void Animate(float dt) { camera.Animate(dt); }
 };
 
 GPUProgram gpuProgram;
@@ -376,8 +325,10 @@ Scene scene;
 const char* vertexSource = R"(
 	#version 330
     precision highp float;
+
 	layout(location = 0) in vec2 cVertexPosition;
 	out vec2 texcoord;
+
 	void main() {
 		texcoord = (cVertexPosition + vec2(1, 1))/2;
 		gl_Position = vec4(cVertexPosition.x, cVertexPosition.y, 0, 1);
@@ -387,9 +338,11 @@ const char* vertexSource = R"(
 const char* fragmentSource = R"(
 	#version 330
     precision highp float;
+
 	uniform sampler2D textureUnit;
 	in  vec2 texcoord;
 	out vec4 fragmentColor;
+
 	void main() {
 		fragmentColor = texture(textureUnit, texcoord);
 	}
@@ -424,6 +377,7 @@ FullScreenTexturedQuad* fullScreenTexturedQuad;
 void onInitialization() {
     glViewport(0, 0, windowWidth, windowHeight);
     scene.build();
+    scene.Animate(0.2f);
     std::vector<vec4> image(windowWidth * windowHeight);
     long timeStart = glutGet(GLUT_ELAPSED_TIME);
     scene.render(image);
@@ -447,4 +401,8 @@ void onKeyboard(unsigned char key, int pX, int pY) {}
 void onKeyboardUp(unsigned char key, int pX, int pY) {}
 void onMouse(int button, int state, int pX, int pY) {}
 void onMouseMotion(int pX, int pY) {}
-void onIdle() {}
+
+void onIdle() {
+    scene.Animate(0.05f);
+    glutPostRedisplay();
+}
